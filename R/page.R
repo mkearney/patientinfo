8a6a7a69e1241833a82263c8072c3d6d9571200a
Michael W. Kearney <kearneymw@missouri.edu>
2019-02-25 10:50:13 -0600

    Add page number functions

--- R/page.R
+++ R/page.R
@@ -7,8 +7,41 @@
 #' @export
 scrape_patient_forum_page <- function(url) {
   x <- xml2::read_html(url)
+  if (is_multi_page(x)) {
+    pn <- get_page_numbers(x)
+    urls <- paste0(url, "?page=", pn)
+    #lapply(urls, convert_page_to_data_frae=me)
+    x <- x
+  } else {
+    x <- x
+  }
   x
 }
+
+`%P%` <- function(lhs, rhs) paste0(lhs, rhs)
+
+read_web_page <- function(x) xml2::read_html(x)
+
+n_uq <- function(x) length(unique(x))
+
+is_multi_page <- function(x) {
+  !is.na(determine_page_count(x))
+}
+
+determine_page_count <- function(x) {
+  x %>%
+    rvest::html_node(".reply__control.reply-pagination") %>%
+    rvest::html_text()
+}
+
+get_page_numbers <- function(x) {
+  p <- x %>%
+    rvest::html_node(".reply__control.reply-pagination") %>%
+    rvest::html_text()
+  m <- gregexpr("\\d+(?=/)", p, perl = TRUE)
+  regmatches(p, m)[[1]]
+}
+
 
 #' Scrape pages
 #'
